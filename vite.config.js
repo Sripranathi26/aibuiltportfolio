870ccbde42f81b12e3a7d4ffada84c1a4b214145
Sripranathi26 <141115673+Sripranathi26@users.noreply.github.com>
2025-08-27 00:49:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Change "your-repo-name" to your GitHub repo name
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/Sripranathi26/aibuiltportfolio', 
 })
